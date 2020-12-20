6961b743813e1971cdc3d249c124e4aa5df53a62
Xuan Huy <macbookpro@XuanHuy.local>
2020-12-20 20:18:06 +0700

    add file Session 2

---
+++ Session 1/Homework/hwday1.js
@@ -0,0 +1,30 @@
+// Câu 3
+let message = 'Coding is great';
+console.log(message);
+let studentCount = 0;
+console.log(studentCount);
+// Câu 4
+let message = 'Coding might not be easy, but still great'; 
+console.log(message);
+let studentCount = 10; 
+console.log(studentCount);
+console.log(message.toLowerCase());
+studentCount++; console.log(studentCount);
+// Câu 5
+alert('Hi, have a good day.');
+// Câu 6
+let userName = prompt('Hi what is your name ?');
+alert(`Hi ${userName}`);
+// Câu 7
+let userFirstName; userFirstName = prompt('What is your first name ?');
+let userLastName; userLastName = prompt('What is your first name ?');
+alert(`Hi, ${userLastName} ${userFirstName}`);
+// Câu 8
+let squareSideLength = prompt('Enter square side length:');
+alert(`The square area is ${squareSideLength*squareSideLength}`);
+// Câu 9
+let circleRadius = prompt('Enter circle radius: ');
+alert(`The circle area is ${circleRadius*circleRadius*3.14}`);
+// Câu 10
+let celsiusDegree = prompt('Enter the temperature in Celsius:');
+alert(`${celsiusDegree} (C) = ${celsiusDegree*1.8+32} (F)`);
